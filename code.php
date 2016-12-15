f3039b984906fb7cca9fca5fdfd873d78c558333
Luiz Fernando Peres <luiz_peres10@hotmail.com>
2016-12-15 12:10:00 -0800

    Improved a bit

--- code.php
+++ code.php
@@ -1,16 +1,19 @@
 <?php
+	header("Access-Control-Allow-Origin: *");
+	header('Content-Type:application/json; charset=UTF-8');
 	include "xml.php";
 
-	$name = @$_POST["name"]; 
+	$name = @$_POST["name"];
 	$msg = @$_POST["message"];
 	$type = @$_POST["type"];
-	$lastCounter = @$_POST["counter"]; 
+	$lastCounter = @$_POST["counter"];
 
 	$fileManager =  new XMLManager();
 
 	if ($type == "send")
 	{
 		$fileManager->insert($name, $msg);
+		echo json_encode(array("status" => "success"));
 	}
 	else if ($type == "get")
 	{
